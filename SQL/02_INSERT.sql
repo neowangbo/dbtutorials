156c601e3c53a9ff801b8d0bbca4a5f1de79812a
Neo <neo.wangbo@gmail.com>
2016-10-28 23:39:45 +0800

    INSERT

--- SQL/02_INSERT.sql
+++ SQL/02_INSERT.sql
@@ -9,6 +9,7 @@ gender VARCHAR2(1) NOT NULL
 --Case 1.Copy all rows of authors table to authors_bak table
 INSERT INTO authors_bak (id,name,birthday,gender) SELECT id,name,birthday,gender FROM authors;
 COMMIT;
+
 --Case 2.Insert a record
 INSERT INTO authors (id,name,birthday,gender)
 SELECT 4,'Dang Nian Ming Yue',to_date('19791127','YYYYMMDD'),'M' FROM dual;
@@ -16,9 +17,16 @@ COMMIT;
 
 --Case 3.
 INSERT INTO authors_bak (id,name,birthday,gender) 
-SELECT id,name,birthday,gender 
+SELECT id,
+       name,
+       birthday,
+       gender 
 FROM authors A
-WHERE not exits ( SELECT AB.id,AB.name,AB.birthday,AB.gender FROM authors_bak AB WHERE AB.id=A.id );
+WHERE NOT EXISTS ( 
+SELECT B.id,
+       B.name,
+       B.birthday,
+       B.gender 
+FROM authors_bak B 
+WHERE B.id=A.id );
 COMMIT;
-
-SELECT 1 FROM authors WHERE id=1;
